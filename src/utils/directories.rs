341acae58d83b4cab37844742f75d0b9ae2eb1ed
Jérémie Bolduc <jeremiebolducjs@gmail.com>
2023-11-28 14:04:32 -0500

    feat: added directories utility

--- src/utils/directories.rs
+++ src/utils/directories.rs
@@ -2,14 +2,12 @@ use std::error::Error;
 use std::fs;
 use std::path::{Path, PathBuf};
 
-use crate::utils::constants::APP_NAME;
+use super::paths::get_app_path;
 
-pub fn find_directories(query: &str) -> Result<Vec<PathBuf>, Box<dyn Error>> {
-    let home_dir = dirs::home_dir().ok_or("Unable to determine home directory")?;
-    let app_path = home_dir.join(APP_NAME);
+pub fn find_directories(query: &str, from: &Path) -> Result<Vec<PathBuf>, Box<dyn Error>> {
     let mut result_directories = Vec::new();
 
-    find_directories_recursive(&app_path, query, &mut result_directories)?;
+    find_directories_recursive(from, query, &mut result_directories)?;
 
     Ok(result_directories)
 }
@@ -25,15 +23,21 @@ fn find_directories_recursive(
 
         if path.is_dir() {
             if path.display().to_string().contains(search_string) {
-                let home_dir = dirs::home_dir().ok_or("Unable to determine home directory")?;
-                let app_path = home_dir.join(APP_NAME);
+                let app_path = get_app_path();
                 let relative_path = path.strip_prefix(app_path)?.to_path_buf();
+
                 result_directories.push(relative_path);
             }
 
             find_directories_recursive(&path, search_string, result_directories)?;
         }
     }
+
+    Ok(())
+}
+
+pub fn create_directories(path: &Path) -> Result<(), std::io::Error> {
+    fs::create_dir_all(path)?;
 
     Ok(())
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,4 @@
 pub mod constants;
 pub mod find;
+pub mod directories;
 pub mod paths;
