39a55fb67c861f5255889af0faef337e482f938e
Jeremie Bolduc <jeremiebolducjs@gmail.com>
2023-11-30 02:17:38 -0500

    feat: adde init command

---
+++ src/cli/init.rs
@@ -0,0 +1,40 @@
+use async_trait::async_trait;
+use clap::Parser;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+
+use crate::utils::constants::GPG_RECIPIENT_FILENAME;
+use crate::utils::paths::get_app_path;
+
+use super::command_handler::CommandHandler;
+
+#[derive(Parser, Debug)]
+pub struct InitArgs {
+    gpg_recipient: String,
+}
+
+pub struct InitHandler {
+    gpg_recipient: String,
+}
+
+impl InitHandler {
+    pub fn new(args: InitArgs) -> Self {
+        InitHandler {
+            gpg_recipient: args.gpg_recipient.to_owned(),
+        }
+    }
+}
+
+#[async_trait]
+impl CommandHandler for InitHandler {
+    async fn execute_async(&self) -> Result<Option<String>, Box<dyn Error>> {
+        let app_path = get_app_path();
+        let file_path = app_path.join(GPG_RECIPIENT_FILENAME);
+        let mut file = File::create(file_path)?;
+
+        file.write_all(self.gpg_recipient.as_bytes())?;
+
+        Ok(None)
+    }
+}
